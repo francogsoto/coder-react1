3d5e4ffa6fd573b046754efea2ff8c1a201e3a8a
Franco Guillermo Soto <francogsoto94@gmail.com>
2022-07-11 22:59:45 -0300

    Agregue navbar

---
+++ francogsoto-npx/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+    console.log("Test")
+  return (
+    <div className="NavMenu">NavBar
+        <nav style = {{color: 'white'}}>
+            {/* <img src="https://place-hold.it/50x30?text=LOGO" alt="" width="50" height="30" /> */}
+            <h1>Ecommerce</h1>
+            <ul>
+                <li> <a href="">Inicio</a></li>
+                <li> <a href="">Menu</a>
+                <ul>
+                <li><a href="">Producto1</a></li>
+                <li><a href="">Producto2</a></li>
+                </ul>
+                </li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
