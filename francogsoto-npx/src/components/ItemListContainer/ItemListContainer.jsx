6c06213caed7bbc4f4b1d1681a959c181f67d3c7
Franco Guillermo Soto <francogsoto94@gmail.com>
2022-07-18 18:19:01 -0300

    Corregi itemlistconainer

--- francogsoto-npx/src/components/ItemListContainer/ItemListContainer.jsx
+++ francogsoto-npx/src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const ItemListContainer = (props) => {
   return (
-    <div  className="saludoStyle">
+    <>
         <h2>{props.saludo}</h2>
-    </div>
+    </>
   )
 }
 
